3c43c29deb4478b0324c2eb363bf35c97bae5471
Grant Tremblay <granttremblay@gmail.com>
2020-09-04 10:18:14 -0400

    update

--- Scripts/plot_current_by_temp.py
+++ Scripts/plot_current_by_temp.py
@@ -25,32 +25,28 @@ fetch.data_source.set('cxc', 'maude allow_subset={}'.format(allow_subset))
 
 
 # PLOT RELEVANT TEMPERATURES
-with fetch.data_source('maude'):
+with fetch.data_source('maude allow_subset=False'):
     dat = fetch.MSIDset(['2FHTRMZT', '2CHTRPZT', '2PRBSCR'],
-                        '2020:232')
+                        '2020:245')
 dat.interpolate()
 # intersection_indices = np.intersect1d(
 #     dat['2PRBSCR'].times, dat['2FHTRMZT'].times, return_indices=True)
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(20, 10))
 
-ax.plot(dat['2PRBSCR'].vals, dat['2FHTRMZT'].vals,
-        color=yellow, markersize=markersize)
-# ax.plot_date(hrc.convert_chandra_time(
-#     dat['2CHTRPZT'].times), dat['2CHTRPZT'].vals/20, label='2CHTRPZT', color=purple, markersize=markersize)
-# ax.plot_date(hrc.convert_chandra_time(
-#     dat['2PRBSCR'].times), dat['2PRBSCR'].vals, label='2PRBSCR', color=blue, markersize=markersize)
-ax.legend()
-# dat['2FHTRMZT'].plot(label='2FHTRMZT', color=yellow)
-# dat['2CHTRPZT'].plot(label='2CHTRPZT', color=purple)
-# dat['2PRBSCR'].plot(label='2PRBSCR', color=blue)
+x = dat['2CHTRPZT'].vals-273.15
+y = dat['2PRBSCR'].vals
 
-# ax.axvline(eventdate, color='gray')
-# ax.axvline(hrc_poweroff_date, color='gray')
-# ax.axvline(cap_step_2, color='gray')
-# ax.axvline(time_of_second_anomaly, color='gray')
-# ax.axvline(time_of_second_shutdown, color='gray')
-# ax.axvline(time_of_cap_1543, color='gray')
-ax.legend()
+
+ax.scatter(x, y)
+# Fit a best-fit
+m, b = np.polyfit(x, y, 1)
+ax.plot(x, m*x + b, color=blue)
+
+ax.set_xlabel('CEA Box Temp 2CHTRPZT (C)')
+ax.set_ylabel('S/C Redundant Bus Current 2PRBSCR (A)')
+plt.title('Data Following Side B Swap')
+
+ax.text(20, 1.9, 'Slope of best fit is {} mA / C'.format(np.round(m, 4)), color='dimgray')
 plt.tight_layout()
 plt.show()
