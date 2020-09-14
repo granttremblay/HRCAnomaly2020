e8899ebcf9782beff584017cf7163be7a8b3ff3f
Grant Tremblay <granttremblay@gmail.com>
2020-09-14 13:00:03 -0400

    making this code agnostic to hrcsentinel

--- Scripts/status_dashboard.py
+++ Scripts/status_dashboard.py
@@ -42,14 +42,48 @@ plt.rcParams['xtick.labelsize'] = labelsizes
 plt.rcParams['ytick.labelsize'] = labelsizes
 
 
+def convert_chandra_time(rawtimes):
+    """
+    Convert input CXC time (seconds since 1998.0) to the time base required for the matplotlib
+    plot_date function (days since start of the Year 1 A.D).
+    """
+
+    # rawtimes is in units of CXC seconds, or seconds since 1998.0
+    # Compute the Delta T between 1998.0 (CXC's Epoch) and 1970.0 (Unix Epoch)
+
+    seconds_since_1998_0 = rawtimes[0]
+
+    cxctime = dt.datetime(1998, 1, 1, 0, 0, 0)
+    unixtime = dt.datetime(1970, 1, 1, 0, 0, 0)
+
+    # Calculate the first offset from 1970.0, needed by matplotlib's plotdate
+    # The below is equivalent (within a few tens of seconds) to the command
+    # t0 = Chandra.Time.DateTime(times[0]).unix
+    delta_time = (cxctime - unixtime).total_seconds() + seconds_since_1998_0
+
+    plotdate_start = mdate.epoch2num(delta_time)
+
+    # Now we use a relative offset from plotdate_start
+    # the number 86,400 below is the number of seconds in a UTC day
+
+    chandratime = (np.asarray(rawtimes) -
+                   rawtimes[0]) / 86400. + plotdate_start
+
+    return chandratime
+
+
 def convert_to_doy(datetime_start):
     '''
-    Return a string like '2020:237' that will be passed to start= in fetch.get_telem()
+    Return a string like '2020:237' that will be passed to start= in fetch.get_telem() and fetch.MSID(). 
+    Note that you have to zero-pad the day number e.g. 2020:002 instead of 2020:2, otherwise
+    the pull will fail. 
     '''
 
     year = datetime_start.year
     day_of_year = datetime_start.timetuple().tm_yday
-    doystring = '{}:{}'.format(year, day_of_year)
+
+    # you have to zero-pad the day number!
+    doystring = '{}:{:03d}'.format(year, day_of_year)
     print(doystring)
 
     return doystring
@@ -69,17 +103,17 @@ def update_plot(counter, plot_start=dt.datetime(2020, 8, 31, 00), plot_end=dt.da
                 # Fetch the telemetry
                 if missionwide is False:
                     data = fetch.get_telem(
-                        msid, start=convert_to_doy(plot_start))
+                        msid, start=convert_to_doy(plot_start), sampling='full', max_fetch_Mb=100000, max_output_Mb=100000)
                     date_format = mdate.DateFormatter('%d %H')
                 elif missionwide is True:
                     print('hello')
                     data = fetch.get_telem(
-                        msid, start='2000:001', sampling='daily', max_fetch_Mb=10000, max_output_Mb=10000)
+                        msid, start=plot_start, sampling='daily', max_fetch_Mb=100000, max_output_Mb=100000)
                     date_format = mdate.DateFormatter('%y-%m')
                 # Allow printing again
                 sys.stdout = sys.__stdout__
 
-                ax.plot_date(hrc.convert_chandra_time(
+                ax.plot_date(convert_chandra_time(
                     data[msid].times), data[msid].vals, markersize=1, label=msid)
 
                 if missionwide is True:
@@ -102,7 +136,8 @@ def update_plot(counter, plot_start=dt.datetime(2020, 8, 31, 00), plot_end=dt.da
 if __name__ == "__main__":
 
     # Must be a DATETIME object! Beware, anything like datetime.now() will return local time, not UTC!
-    plot_start = dt.datetime(2020, 9, 8, 12)
+    # plot_start = dt.datetime(2020, 9, 8, 12)
+    plot_start = dt.datetime(2000, 1, 1)
     plot_end = dt.date.today() + dt.timedelta(days=2)
 
     fig_save_directory = '/Users/grant/HEAD/data/wdocs/tremblay/HRCOps/plots/'
@@ -111,6 +146,7 @@ if __name__ == "__main__":
     plt.figure(0, figsize=(16, 7))
     # plt.tight_layout()
     counter = 0
+
     while True:
         fetch.data_source.set('maude')
         print("Refreshing dashboard (iteration {}) at {}".format(
@@ -127,7 +163,7 @@ if __name__ == "__main__":
             print(ex)
 
         fetch.data_source.set('cxc')
-        update_plot(counter, plot_start=dt.datetime(2000, 1, 1),
+        update_plot(counter, plot_start=dt.datetime(2006, 1, 1),
                     plot_end=None, missionwide=True)
         plt.tight_layout()
         plt.draw()
