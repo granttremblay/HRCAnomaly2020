6f3482f8a749a9d8c44ad96ef914cb9a8a579319
Grant Tremblay <granttremblay@gmail.com>
2020-10-15 14:15:05 -0400

    update

--- Scripts/plot_all_motors.py
+++ Scripts/plot_all_motors.py
@@ -25,27 +25,37 @@ from plot_stylers import *
 
 from hrcsentinel import hrccore as hrc
 
-hrc.styleplots()
+
+plt.style.use('ggplot')
+labelsizes = 8
+plt.rcParams['font.sans-serif'] = 'Arial'
+plt.rcParams['font.size'] = labelsizes
+
+
+plt.rcParams['axes.titlesize'] = labelsizes
+plt.rcParams['axes.labelsize'] = labelsizes
+plt.rcParams['xtick.labelsize'] = labelsizes-2
+plt.rcParams['ytick.labelsize'] = labelsizes
 
 fetch.data_source.set('maude allow_subset=True')
 
-fig, ax = plt.subplots()
+
 counter = 0
 
 
 n_lines = len(motor_msids)
 color_idx = np.linspace(0, 1, n_lines)
 
-
+fig, ax = plt.subplots()
 for i, msid in zip(color_idx, motor_msids):
     print('Fetching {}'.format(msid))
     dat = fetch.MSID(msid, start='2020:220')
     # YOU HAVE TO USE RAW_VALS HERE BECAUSE OTHERWISE VALS WILL BEA  STRING LIKE UNACT OR ENAB
     try:
         ax.plot_date(hrc.convert_chandra_time(dat.times),
-                     dat.raw_vals + counter, label=dat.msid, color=plt.cm.tab20(i), markersize=0, linestyle='-', linewidth=1.5)
+                     dat.raw_vals + counter, label=dat.msid, color=plt.cm.viridis(i), markersize=0, linestyle='-', linewidth=1.5)
         ax.text(dt.datetime(2020, 8, 10, 0, 0),
-                dat.raw_vals[0] + counter + 0.1, dat.msid, color=plt.cm.tab20(i), size=10)
+                dat.raw_vals[0] + counter + 0.1, dat.msid, color=plt.cm.viridis(i), size=10)
     except Exception as inst:
         print('ERROR on {}: '.format(msid, inst))
 
@@ -64,3 +74,33 @@ ax.axvline(time_of_cap_1543, color='gray')
 
 
 plt.show()
+
+for i, msid in zip(color_idx, motor_msids):
+    fig, ax = plt.subplots()
+    print('Fetching {}'.format(msid))
+    dat = fetch.MSID(msid, start='2020:220')
+    # YOU HAVE TO USE RAW_VALS HERE BECAUSE OTHERWISE VALS WILL BEA  STRING LIKE UNACT OR ENAB
+
+    ax.set_ylim(-2, 2)
+    ax.set_xlim(dt.datetime(2020, 8, 22, 0, 0), dt.datetime(2020, 9, 2, 0, 0))
+    ax.axvline(eventdate, color='gray')
+    ax.axvline(hrc_poweroff_date, color='gray')
+    ax.axvline(a_side_reset, color='gray')
+
+    ax.axvline(time_of_second_anomaly, color='gray')
+    ax.axvline(time_of_second_shutdown, color='gray')
+
+    ax.axvline(time_of_cap_1543, color='gray')
+    try:
+        ax.plot_date(hrc.convert_chandra_time(dat.times),
+                     dat.raw_vals, label=dat.msid, color=plt.cm.viridis(i), markersize=0, linestyle='-', linewidth=1.5)
+        ax.text(dt.datetime(2020, 8, 10, 0, 0),
+                dat.raw_vals[0] + 0.1, dat.msid, color=plt.cm.viridis(i), size=10)
+    except Exception as inst:
+        print('ERROR on {}: '.format(msid, inst))
+
+    ax.set_ylim(-2, 2)
+    ax.set_xlim(dt.datetime(2020, 8, 22, 0, 0), dt.datetime(2020, 9, 2, 0, 0))
+
+    fig.savefig('/Users/grant/Desktop/{}.png'.format(msid), dpi=300)
+    plt.close()
