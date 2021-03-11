c456cd27f3238c6adc4760e64711d0964fd3f2bb
Grant Tremblay <granttremblay@gmail.com>
2021-03-11 10:13:37 -0500

    asdf

--- Excel/generate_anomaly_master_excel_sheet.py
+++ Excel/generate_anomaly_master_excel_sheet.py
@@ -21,7 +21,6 @@ import numpy as np
 import pandas as pd
 from astropy.table import Table
 
-from hrcsentinel import hrccore as hrc
 
 from tqdm import tqdm as progressbar
 
@@ -39,6 +38,41 @@ def convert_to_doy(datetime_start):
     doystring = '{}:{}'.format(year, day_of_year)
 
     return doystring
+
+
+
+def convert_chandra_time(rawtimes):
+    """
+    Convert input CXC time (sec) to the time base required for the matplotlib
+    plot_date function (days since start of the Year 1 A.D - yes, really).
+    :param times: iterable list of times, in units of CXCsec (sec since 1998.0)
+    :rtype: plot_date times (days since Year 1 A.D.)
+    """
+
+    # rawtimes is in units of CXC seconds, or seconds since 1998.0
+    # Compute the Delta T between 1998.0 (CXC's Epoch) and 1970.0 (Unix Epoch)
+
+    seconds_since_1998_0 = rawtimes[0]
+
+    cxctime = dt.datetime(1998, 1, 1, 0, 0, 0)
+    unixtime = dt.datetime(1970, 1, 1, 0, 0, 0)
+
+    # Calculate the first offset from 1970.0, needed by matplotlib's plotdate
+    # The below is equivalent (within a few tens of seconds) to the command
+    # t0 = Chandra.Time.DateTime(times[0]).unix
+    delta_time = (cxctime - unixtime).total_seconds() + seconds_since_1998_0
+
+    plotdate_start = mdate.epoch2num(delta_time)
+
+    # Now we use a relative offset from plotdate_start
+    # the number 86,400 below is the number of seconds in a UTC day
+
+    chandratime = (np.asarray(rawtimes) -
+                   rawtimes[0]) / 86400. + plotdate_start
+
+    return chandratime
+
+
 
 
 def msids():
@@ -75,15 +109,15 @@ def msids():
 def main():
 
     data_start = convert_to_doy(dt.datetime(2020, 8, 23, 0, 0))
-    data_stop = convert_to_doy(dt.datetime(2020, 8, 29, 0, 0))
+    data_stop = convert_to_doy(dt.datetime(2020, 8, 28, 0, 0))
 
     temperature_msids = msids()
 
-    dat = fetch.MSIDset(temperature_msids, start=data_start,
+    dat = fetch.get_telem(temperature_msids, start=data_start,
                         stop=data_stop)
 
     data_columns = [dat['2CEAHVPT'].times,
-                    mdate.num2date(hrc.convert_chandra_time(dat['2CEAHVPT'].times))]
+                    mdate.num2date(convert_chandra_time(dat['2CEAHVPT'].times))]
 
     column_names = ['Chandra Time',
                     'Human-readable time (UTC)']
